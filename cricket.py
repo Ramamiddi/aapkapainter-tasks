22ba69e5b67984a4c411ef107ad2df9df3e1a965
Ramamiddi <35324680+Ramamiddi@users.noreply.github.com>
2022-03-03 23:08:13 +0530

    Update cricket.py

--- cricket.py
+++ cricket.py
@@ -1,1 +1,10 @@
-
+score1 = [1,2,3,4,2]
+p1=[]
+p2=[]
+for i in score1:
+    if i % 2 != 0:
+        p1.append(i)
+    else:
+        p2.append(i)
+print("P1 is:", sum(p1))
+print("P2 is:", sum(p2))
